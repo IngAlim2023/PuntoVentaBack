48ca2cd35568c1ab44538426eb2eb4344e512632
IngAlim2023 <enrique.porras@udea.edu.co>
2025-02-04 11:15:56 -0500

    API CORREGIDA

--- src/config/server.js
+++ src/config/server.js
@@ -5,7 +5,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
-import productoRoute from "../modules/productos/routes/productoRoute.js"
+import productoRoute from "../modules/productos/routes/productoRoute.js";
 import ventaRoute from "../modules/ventas/routes/ventaRoute.js";
 import usuarioRoute from "../modules/usuarios/routes/usuarioRoute.js";
 
@@ -18,11 +18,9 @@ const __dirname = path.dirname(__filename);
 const uploadsPath = path.join(__dirname, "../uploads");
 
 const corsOptions = {
-    origin: [
-        "http://localhost:5173",
-    ],
-    credentials: true
-}
+  origin: ["http://localhost:5173", process.env.URL_API],
+  credentials: true,
+};
 
 const port = 3000;
 
@@ -32,11 +30,10 @@ appBackend.use(cors(corsOptions));
 appBackend.use(morgan("dev"));
 appBackend.use(express.json());
 
-appBackend.use('/api/v1/productos', productoRoute);
-appBackend.use('/api/v1/ventas', ventaRoute);
-appBackend.use('/api/v1/usuarios', usuarioRoute);
+appBackend.use("/api/v1/productos", productoRoute);
+appBackend.use("/api/v1/ventas", ventaRoute);
+appBackend.use("/api/v1/usuarios", usuarioRoute);
 // Sirve la carpeta de imágenes
 appBackend.use("/uploads", express.static(uploadsPath));
-
 
 appBackend.set("port", process.env.PORT || port);
